d1224fe7f12be4e1e69c30e1ad73f622c83e803a
Hossam Ramadan <103776358+HossamRamadan667@users.noreply.github.com>
2025-04-30 22:16:57 +0300

    create test Login screen
    - added test Login screen
    - add login route

--- lib/src/core/routing/router.dart
+++ lib/src/core/routing/router.dart
@@ -1,4 +1,5 @@
 import 'package:advanced_flutter/src/core/routing/routes.dart';
+import 'package:advanced_flutter/src/features/login/ui/login_screen.dart';
 import 'package:advanced_flutter/src/features/onboarding/ui/onboarding_screen.dart';
 
 import 'package:flutter/material.dart';
@@ -9,6 +10,8 @@ class AppRouter {
       case AppRoutes.onboarding:
         return MaterialPageRoute(
             builder: (context) => const OnboardingScreen());
+      case AppRoutes.login:
+        return MaterialPageRoute(builder: (context) => const Login());
       default:
         return MaterialPageRoute(
           builder: (context) => Center(
